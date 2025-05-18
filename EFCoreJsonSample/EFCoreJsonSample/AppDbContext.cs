827063da49473e7d7626fefcd383bfe73c9ecb5b
mehmetemineker <mehmetemineker92@gmail.com>
2025-05-18 20:09:10 +0300

    feat: add PostgreSQL support and product retrieval endpoints

---
+++ EFCoreJsonSample/EFCoreJsonSample/AppDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreJsonSample
+{
+    public class AppDbContext(DbContextOptions opts) : DbContext(opts)
+    {
+        public DbSet<Product> Products { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Product>()
+        //        .Property(p => p.Details)
+        //        .HasColumnType("jsonb")
+        //        .HasConversion(
+        //            v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+        //            v => JsonSerializer.Deserialize<ProductDetails>(v, (JsonSerializerOptions)null));
+        //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .OwnsOne(p => p.Details)
+                .ToJson();
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public ProductDetails Details { get; set; } = null!;
+    }
+
+    [Owned]
+    public class ProductDetails
+    {
+        public string Manufacturer { get; set; } = null!;
+        public string WarrantyPeriod { get; set; } = null!;
+        public List<string> Tags { get; set; } = null!;
+    }
+}
--- EFCoreJsonSample/EFCoreJsonSample/Program.cs
+++ EFCoreJsonSample/EFCoreJsonSample/Program.cs
@@ -1,6 +1,23 @@
+using EFCoreJsonSample;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/products", async (AppDbContext db) =>
+{
+    var products = await db.Products
+    .Where(p => p.Details.Manufacturer == "Sample")
+    .ToListAsync();
+
+    return Results.Ok(products);
+});
 
 app.UseHttpsRedirection();
 
